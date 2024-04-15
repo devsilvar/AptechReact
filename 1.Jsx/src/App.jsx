56eeb9ce233b4beb7028bfbc6f055e1a98fbe4fe
Yusuf <refsilva@yahoo.com>
2024-04-15 22:26:28 +0100

    first react class

---
+++ 1.Jsx/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return 'hello';
+
+  // --steps
+  // React DOm creates a Root
+  // The id root inside the HTML is located and selected
+  // The app componenet is rendered  in main.jsx
+  // The result of the rendering is sent to the Root   - index.html
+}
+export default App;
